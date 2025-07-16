ce73595f5250a246a37f5ec72165e997ac250e9a
ZackLackinJava <brokeskibusiness@gmail.com>
2025-07-16 16:40:02 +0300

    Add Optimizer interface and implement SGD with constant learning rate

--- src/main/java/me/brokeski/training/Optimizer.java
+++ src/main/java/me/brokeski/training/Optimizer.java
@@ -1,4 +1,18 @@
 package me.brokeski.training;
 
-public class Optimizer {
+/**
+ * Optimizer interface for updating model parameters during training.
+ * Intended to be passed into layers or the trainer.
+ *
+ * Produced in 2025
+ */
+
+public interface Optimizer {
+    /**
+     * Returns the current learning rate, which may change over time.
+     *
+     * @param currentEpoch The current training epoch (starting at 1).
+     * @return The learning rate to use.
+     */
+    double getLearningRate(int currentEpoch);
 }
--- src/main/java/me/brokeski/training/SGD.java
+++ src/main/java/me/brokeski/training/SGD.java
@@ -1,4 +1,26 @@
 package me.brokeski.training;
 
-public class SGD {
+/**
+ * Stochastic Gradient Descent (SGD) optimizer with constant learning rate.
+ *
+ * 2025
+ */
+
+public class SGD implements Optimizer{
+
+    private final double learningRate;
+
+    /**
+     * Constructs a basic SGD optimizer.
+     *
+     * @param learningRate The fixed learning rate to use.
+     */
+    public SGD(double learningRate) {
+        this.learningRate = learningRate;
+    }
+
+    @Override
+    public double getLearningRate(int currentEpoch) {
+        return learningRate;
+    }
 }
