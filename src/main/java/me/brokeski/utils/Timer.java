8e5444d312253bfe25f7c932def6598cab574fb2
ZackLackinJava <brokeskibusiness@gmail.com>
2025-07-16 16:56:41 +0300

    Add Timer utility to measure elapsed time in ms or seconds

--- src/main/java/me/brokeski/utils/Timer.java
+++ src/main/java/me/brokeski/utils/Timer.java
@@ -1,4 +1,38 @@
 package me.brokeski.utils;
 
+/**
+ * Timer utility to measure elapsed time in milliseconds or seconds.
+ * Can be used to benchmark operations like forward passes or training epochs.
+ *
+ * 2025
+ */
+
 public class Timer {
+
+    private long startTime;
+
+    /**
+     * Starts or restarts the timer.
+     */
+    public void start(){
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Returns the elapsed time since start in milliseconds.
+     *
+     * @return Elapsed time in milliseconds.
+     */
+    public long elapsedMillis(){
+        return (System.nanoTime() - startTime) / 1000000;
+    }
+
+    /**
+     * Returns the elapsed time since start in seconds.
+     *
+     * @return Elapsed time in seconds (as a double).
+     */
+    public double elapsedSeconds(){
+        return (System.nanoTime()-startTime) / 1000000000.0;
+    }
 }
